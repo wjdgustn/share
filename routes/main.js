8288102d0058fc86bdead52eaae4911311e1f8a2
Hyunsoo, Chung <admin@hyonsu.com>
2020-11-28 23:19:19 +0900

    Add file limit pass

--- routes/main.js
+++ routes/main.js
@@ -30,8 +30,12 @@ app.get('/', limiter, (req, res, next) => {
     return res.render('main');
 });
 
+app.get('/check', (req, res, next) => {
+    return res.send(setting.BYPASS_LIMIT.includes(res.locals.ip) ? '용량, rate limit 제한을 받지 않습니다.' : '용량, rate limit 제한을 받습니다.');
+});
+
 app.post('/upload', upload.single('file'), async (req, res, next) => {
-    if(req.file.size > 1024 * 1024 * 1024) {
+    if(!setting.BYPASS_LIMIT.includes(res.locals.ip) && req.file.size > 1024 * 1024 * 1024) {
         req.flash('Error', '파일이 1GB를 초과합니다.');
         return res.redirect('/');
     }
